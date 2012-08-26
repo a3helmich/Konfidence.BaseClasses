fe78da79874faab095bbfb44871cca99b401a1d7
A3 <KONFIDENCE\A3>
2012-08-26 15:45:18 +0200

    ingecheckt met de team foundation library

---
+++ BaseClassesKonfidenceClasses/Generated.Sql/Test4_GetRow.sql
@@ -0,0 +1,19 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[gen_Test4_GetRow]') AND type IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[gen_Test4_GetRow]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[gen_Test4_GetRow]
+(
+	@Id int
+)
+AS
+	SELECT *
+	FROM [Test4]
+	WHERE [Id] = @Id
+	
+RETURN
