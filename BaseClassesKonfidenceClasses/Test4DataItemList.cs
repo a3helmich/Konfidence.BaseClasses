d2eabbc2e368053be501cc9bb1e5730e6f359d73
A3 <KONFIDENCE\A3>
2012-08-28 22:19:30 +0200

    ingecheckt met de team foundation library

---
+++ BaseClassesKonfidenceClasses/Test4DataItemList.cs
@@ -0,0 +1,12 @@
+using System;
+using Konfidence.BaseData;
+
+namespace Konfidence.TestClassGeneratorClasses
+{
+	public partial class Bl
+	{
+		public partial class Test4DataItemList : BaseDataItemList<Test4DataItem>
+		{
+		}
+	}
+}
