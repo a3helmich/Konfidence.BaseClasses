03963ebdf8391001fe74146dcb748a1de569b97b
A3 <KONFIDENCE\A3>
2012-12-10 11:23:06 +0100

    ingecheckt met de team foundation library

---
+++ Konfidence.MenuClasses/MenuTextDataItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Data;
+using Konfidence.BaseData;
+
+namespace Konfidence.DbSiteMapMenuClasses
+{
+	public partial class Bl
+	{
+		public partial class MenuTextDataItem : BaseDataItem
+		{
+		}
+	}
+}
