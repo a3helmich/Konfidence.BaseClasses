3d58286bcb85124d0141fc010b1fcff895eccb64
A3 <KONFIDENCE\A3>
2012-12-10 11:23:06 +0100

    ingecheckt met de team foundation library

---
+++ Konfidence.MenuClasses/Generated.Classes/MenuTextDataItemList.generated.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Data;
+using Konfidence.BaseData;
+
+namespace Konfidence.DbSiteMapMenuClasses
+{
+	public partial class Bl
+	{
+		public partial class MenuTextDataItemList : BaseDataItemList<MenuTextDataItem>
+		{
+			// partial methods
+			partial void BeforeInitializeDataItemList();
+			partial void AfterInitializeDataItemList();
+			
+			private const string MENUTEXT_GETLIST = "gen_MenuText_GetList";
+			
+			protected MenuTextDataItemList() : base()
+			{
+			}
+			
+			protected sealed override void InitializeDataItemList()
+			{
+				BeforeInitializeDataItemList();
+				
+				GetListStoredProcedure = MENUTEXT_GETLIST;
+				
+				AfterInitializeDataItemList();
+			}
+			
+			static public MenuTextDataItemList GetEmptyList()
+			{
+				MenuTextDataItemList menutextList = new MenuTextDataItemList();
+				
+				return menutextList;
+			}
+			
+			static public MenuTextDataItemList GetList()
+			{
+				MenuTextDataItemList menutextList = new MenuTextDataItemList();
+				
+				menutextList.BuildItemList();
+				
+				return menutextList;
+			}
+			
+			public MenuTextDataItemList FindAll()
+			{
+				MenuTextDataItemList menutextList = new MenuTextDataItemList();
+				
+				foreach (MenuTextDataItem menutext in this)
+				{
+					menutextList.Add(menutext);
+				}
+				
+				return menutextList;
+			}
+		}
+	}
+}
