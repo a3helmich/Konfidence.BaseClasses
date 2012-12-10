3d0a1cd818d2f9348631ca1374b3c1ca9d6add3f
A3 <KONFIDENCE\A3>
2012-12-10 11:33:32 +0100

    ingecheckt met de team foundation library

--- Konfidence.MenuClasses/Generated.Classes/MenuDataItemList.generated.cs
+++ Konfidence.MenuClasses/Generated.Classes/MenuDataItemList.generated.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Data;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
 using Konfidence.BaseData;
 
 namespace Konfidence.DbSiteMapMenuClasses
@@ -13,6 +15,9 @@ namespace Konfidence.DbSiteMapMenuClasses
 			partial void AfterInitializeDataItemList();
 			
 			private const string MENU_GETLIST = "gen_Menu_GetList";
+			private const string MENU_GETLISTBY_MENUCODE = "gen_Menu_GetListByMenuCode";
+			
+			private int _MenuCode = 0;
 			
 			protected MenuDataItemList() : base()
 			{
@@ -43,6 +48,17 @@ namespace Konfidence.DbSiteMapMenuClasses
 				return menuList;
 			}
 			
+			static public MenuDataItemList GetListByMenuCode(int menucode)
+			{
+				MenuDataItemList menuList = new MenuDataItemList();
+				
+				menuList._MenuCode = menucode;
+				
+				menuList.BuildItemList(MENU_GETLISTBY_MENUCODE);
+				
+				return menuList;
+			}
+			
 			public MenuDataItemList FindAll()
 			{
 				MenuDataItemList menuList = new MenuDataItemList();
@@ -53,6 +69,17 @@ namespace Konfidence.DbSiteMapMenuClasses
 				}
 				
 				return menuList;
+			}
+			
+			public override void SetParameters(string storedProcedure, Database database, DbCommand dbCommand)
+			{
+				
+				if (storedProcedure.Equals(MENU_GETLISTBY_MENUCODE))
+				{
+					base.SetParameters(storedProcedure, database, dbCommand);
+					
+					database.AddInParameter(dbCommand, MenuDataItem.MENUCODE, DbType.Int32, _MenuCode);
+				}
 			}
 		}
 	}
