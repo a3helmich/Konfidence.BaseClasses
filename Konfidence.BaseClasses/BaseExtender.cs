42a9ae8465cb0876efe5a226a2b74866147f0e45
A3 <KONFIDENCE\A3>
2016-07-20 21:26:13 +0200

    nuget update

--- Konfidence.BaseClasses/BaseExtender.cs
+++ Konfidence.BaseClasses/BaseExtender.cs
@@ -10,20 +10,44 @@ namespace Konfidence.Base
         {
             var assignedString = assignedObject as string;
 
-            if (assignedObject == null)
-            {
-                return false;
-            }
-
             if (assignedString != null)
             {
                 if (string.IsNullOrWhiteSpace(assignedString))
                 {
                     return false;
                 }
+
+                return true;
+            }
+
+            if (assignedObject == null)
+            {
+                return false;
             }
 
             return true;
+        }
+        public static bool IsAssigned(this DateTime assignedTime)
+        {
+            if (assignedTime > DateTime.MinValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static DateTime StartOfDayTime(this DateTime dateTime)
+        {
+            var afterMidnight = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, DateTimeKind.Utc);
+
+            return afterMidnight;
+        }
+
+        public static DateTime EndOfDayTime(this DateTime dateTime)
+        {
+            var midnight = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, DateTimeKind.Utc);
+
+            return midnight;
         }
 
         [ContractAnnotation("assignedGuid:null => false")]
