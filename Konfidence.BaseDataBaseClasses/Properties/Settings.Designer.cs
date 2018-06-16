0d1f8e62a98434a420537acc5bbfe405c2f44c4d
A3 <adrie@konfidence.nl>
2018-06-16 17:54:54 +0200

    restclient mock interface

--- Konfidence.BaseDataBaseClasses/Properties/Settings.Designer.cs
+++ Konfidence.BaseDataBaseClasses/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace Konfidence.BaseData.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost/RegistrationWebservice/BaseService.asmx")]
-        public string BaseDatabaseClasses_WSBaseHost_WSBaseHostService {
-            get {
-                return ((string)(this["BaseDatabaseClasses_WSBaseHost_WSBaseHostService"]));
-            }
-        }
     }
 }
