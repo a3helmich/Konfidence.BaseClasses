3d855a29cecd6582da908793be7e69bd00ff5329
A3 <adrie@konfidence.nl>
2020-02-08 10:08:29 +0100

    no message

---
+++ Konfidence.MenuClasses.Tests/objects/ItemListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Konfidence.MenuClasses.Tests.interfaces;
+
+namespace Konfidence.MenuClasses.Tests.objects
+{
+    public static class ItemListExtensions
+    {
+        public static void AddItem([NotNull] this List<ITestItemClass> itemList)
+        {
+            itemList.Add(new TestItemClass("testString"));
+        }
+    }
+}
