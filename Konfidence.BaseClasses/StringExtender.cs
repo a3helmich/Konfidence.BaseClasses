398da71c12f50ac950aa98a5c0723a6d2f528bf2
A3 <adrie@konfidence.nl>
2021-02-22 18:46:28 +0100

    one more string extender

--- Konfidence.BaseClasses/StringExtender.cs
+++ Konfidence.BaseClasses/StringExtender.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using JetBrains.Annotations;
 
 namespace Konfidence.Base
@@ -117,6 +119,13 @@ namespace Konfidence.Base
             }
 
             return defaultValue;
+        }
+
+        [NotNull]
+        [UsedImplicitly]
+        public static List<string> TrimList([NotNull] this IEnumerable<string> lines)
+        {
+            return lines.Select(x => x.Trim()).ToList();
         }
     }
 }
