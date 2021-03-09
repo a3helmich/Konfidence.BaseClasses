00745047828f4ca646b9ca41034abb47d4cb0745
A3 <adrie@konfidence.nl>
2021-03-09 11:03:57 +0100

    added basic baseRestClient

---
+++ Konfidence.BaseRest.Client/IRestClientConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Konfidence.BaseRest.Client
+{
+    public interface IRestClientConfig
+    {
+        int PortNr { get; set; }
+
+        string Address { get; set; }
+
+        string BaseRoute { get; set; }
+
+        string Route { get; set; }
+
+        Uri BaseUri();
+
+        Uri Host();
+    }
+}
