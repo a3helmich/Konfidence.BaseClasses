16a819657a8b81d7a826b76401059ffe46c7916c
A3 <adrie@konfidence.nl>
2021-10-10 15:17:24 +0200

    update clientManager for mailaccounts, unittests and packages

---
+++ Tools/ClientSettingsUpdater/MailAccounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSettingsUpdater
+{
+    public class MailAccounts
+    {
+        public List<MailAccount> Accounts = new List<MailAccount>();
+    }
+    public class MailAccount
+    {
+        public string UserName = string.Empty;
+        public string Password = string.Empty;
+        public string Server = string.Empty;
+    }
+}
