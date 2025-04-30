40219325f8e191df36e44b2dc01cba94acd2085c
A3 <adrie@konfidence.nl>
2025-04-30 16:50:08 +0200

    #624: move queue to BaseHttpClient

---
+++ Konfidence.BaseClasses/Wpf/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace Konfidence.Base.Wpf;
+
+public sealed class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    [UsedImplicitly]
+    private bool SetField<T>(
+        ref T field,
+        T value,
+        [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return false;
+        }
+
+        field = value;
+
+        OnPropertyChanged(propertyName);
+
+        return true;
+    }
+}
