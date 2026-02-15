ae9b6165d36b6d97e3d7e7f8bd88b2d65530b3da
A3 <adrie@konfidence.nl>
2026-02-15 11:50:04 +0100

    #1024: [description] timeslot polynomial should also follow the timeslot, including counting the min max, step 5

--- Konfidence.BaseClasses/SerializationExtensions.cs
+++ Konfidence.BaseClasses/SerializationExtensions.cs
@@ -16,6 +16,7 @@ public static class SerializationExtensions
     private static readonly JsonSerializerOptions _serializationOptions;
     private static readonly JsonSerializerOptions _deserializationOptions;
     private static readonly JsonSerializerOptions _caseSensitiveDeserializationOptions;
+    private static readonly JsonSerializerOptions _cloneOptions;
 
     static SerializationExtensions()
     {
@@ -41,6 +42,12 @@ public static class SerializationExtensions
             ReadCommentHandling = JsonCommentHandling.Skip,
             Converters = { stringEnumConverter },
             PropertyNameCaseInsensitive = false
+        };
+
+        _cloneOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters = { stringEnumConverter }
         };
     }
 
@@ -79,12 +86,23 @@ public static class SerializationExtensions
         return deserializedDto.Any();
     }
 
+    private static string CloneSerialize<T>(this T toSerializeDto)
+    {
+        return JsonSerializer.Serialize(toSerializeDto, _cloneOptions);
+    }
+
+    private static bool CloneDeserialize<T>(this string toDeserializeDto, [NotNullWhen(true)] out T? deserializedDto)
+    {
+        deserializedDto = JsonSerializer.Deserialize<T>(toDeserializeDto, _cloneOptions);
+
+        return deserializedDto.IsAssigned();
+    }
 
     // TODO : Consider using a more efficient cloning method if TT is known to be a reference type and supports ICloneable or similar.
     //        use a deep clone library like Force.deepCloner or FastDeepCloner if performance is a concern.
     public static T Clone<T>(this T data)
     {
-        return data.Serialize().Deserialize(out T? clonedData)
+        return data.CloneSerialize().CloneDeserialize(out T? clonedData)
             ? clonedData
             : data;
     }
